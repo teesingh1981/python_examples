9c10b4979be4b88b3c144eb2a13718069e553043
teesingh <teesingh@hotmail.com>
2024-01-02 21:46:24 +0000

    added while loop examples

---
+++ py_while_loops/while_loops.py
@@ -0,0 +1,26 @@
+# While loops 
+# 3 control statements - break, continue, pass 
+
+# while loop with break example 
+# c = 0
+# while c < 5:
+#     c = c + 1
+#     if c == 3:
+#         break
+#     print(c)
+
+# while loop with continue example 
+# c = 0
+# while c < 5:
+#     c = c + 1
+#     if c == 3:
+#         continue
+#     print(c)
+
+# while loop with pass example 
+c = 0
+while c < 5:
+    c = c + 1
+    if c == 3:
+        pass
+    print(c)
