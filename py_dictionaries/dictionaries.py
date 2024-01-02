aa128cb121021ce33368ec96b8a67d391d5c104f
teesingh <teesingh@hotmail.com>
2024-01-02 18:48:48 +0000

    added lists & dictionaries

---
+++ py_dictionaries/dictionaries.py
@@ -0,0 +1,6 @@
+# Dictionaries 
+
+students = {'bob': 12, 'rachel': 13, 'emily': 15}
+print(students['rachel'])
+print(students['bob'])
+
---
+++ py_lists/lists.py
@@ -0,0 +1,31 @@
+# Introduction to lists 
+shopping_list = ['apples', 'oranges', 'bananas', 'cheese']
+
+# Print Shopping List Variable 
+# print(shopping_list)
+
+# Print 3rd item in the list as zero counts as position 1
+# print(shopping_list[2])
+
+# Append blueberries into the list 
+# shopping_list.append('blueberries')
+
+# delete oranges from the list
+# del shopping_list[1]
+
+# Change oranges to cherries
+#shopping_list[0] = 'cherries'
+
+#  Lengeth of list 
+
+print(len(shopping_list))
+
+# Shopping list 2 and add to shopping list 1
+shopping_list_2 = ['bread', 'jam', 'peanut butter']
+print(shopping_list+shopping_list_2)
+
+print(shopping_list)
+
+list_num = [1,7,9,3,256]
+print(max(list_num))
+print(min(list_num))
