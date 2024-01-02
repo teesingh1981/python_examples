02969cc50250798a928347c955b03f6ee7cbf2f5
teesingh <teesingh@hotmail.com>
2024-01-02 22:25:24 +0000

    added function examples

---
+++ py_functions/functions.py
@@ -0,0 +1,19 @@
+# Functions 
+
+# Hello World Function Example 
+# def hello_world():
+#     print('Hello World')
+
+# Input name example 
+
+# def greeting(name):
+#     print('What is your name?')
+#     name = input()
+#     print("Hi " + name + "!")
+# greeting('name')
+
+# Add two numbers together 
+
+# def add(num1, num2):
+#     print(num1 + num2)
+# add(10,15)
