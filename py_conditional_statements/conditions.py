9bd722b2764ee42b0169f63179cc095439703646
teesingh <teesingh@hotmail.com>
2024-01-02 21:29:02 +0000

    added conditional statements

---
+++ py_conditional_statements/conditions.py
@@ -0,0 +1,22 @@
+# Conditional Statements 
+
+if 2 < 3:
+    print('hello')
+else:
+    print('condition not true')
+
+# Relational operators 
+
+# > < >= <= == != 
+    
+age = 17
+if age <= 16:
+    print('You are younger than 16')
+elif age == 16:
+    print('you are 16')
+else:
+    print('you are older than 16')
+
+# Logical operators 
+
+# and / or 
