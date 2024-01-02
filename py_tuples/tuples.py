bbd7b305964fa8beb44a24fa80c6d2f86dbc3d65
teesingh <teesingh@hotmail.com>
2024-01-02 21:20:23 +0000

    added tuples example

---
+++ py_tuples/tuples.py
@@ -0,0 +1,15 @@
+# Tuples values cannot be changed, however you can add additional tuple variables 
+
+tup = ("oranges", "bananas", "apples", "cherries")
+tup2 = (12, 14)
+tup3 = (tup + tup2)
+
+print(tup)
+print(tup2)
+print (tup3)
+
+print(tup[0])
+print(tup[1])
+print(tup[2]) 
+
+print(tup[0:4])
