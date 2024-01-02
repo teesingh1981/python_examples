409bba6770046a53d69d993e872bac32cf448e11
teesingh <teesingh@hotmail.com>
2024-01-02 21:40:00 +0000

    added for loop examples

---
+++ py_for_loops/for_loops.py
@@ -0,0 +1,18 @@
+# For Loops 
+
+# list1 = ['apples', 'bananas', 'cherries']
+# tup1 = (2,6,10) 
+
+# for item in list1:
+#     print(item)
+
+# Print numbers in range example 
+
+# for i in range(0, 10):
+#     print(i)
+
+# For loop within For loop example 
+
+for i in range(0, 5):
+    for j in range(0, 3):
+        print(i * j)
