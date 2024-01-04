ed68890675b814b39bf80ee7af4fab87853a1eeb
teesingh <teesingh@hotmail.com>
2024-01-04 11:11:55 +0000

    added builtin functions

---
+++ py_functions2/py_functions.py
@@ -0,0 +1,13 @@
+# Built-in Py Functions 
+
+# absolute example 
+# print(abs(-23))
+
+# bool example 
+# print(bool(0))
+
+# dir example 
+# print(dir("hello"))
+
+# help example 
+print(help("hello".upper))
